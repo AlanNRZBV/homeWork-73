7ba0dfc7a27859be59b0b4f63af6e0127b4d1a63
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-01-10 15:52:15 +0600

    feat: implemented encode/decode functioonality
    Complete Task-2

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
-const port = 8000
+const encodingWord = 'password';
+const port = 8000;
+
 
 app.get('/hello', (req, res) => {
-  res.send('Hello')
+  res.send('Hello');
 })
-app.listen(port)
+
+app.get('/encode/:password', (req, res) => {
+  res.send(Vigenere.Cipher(encodingWord).crypt(req.params.password));
+})
+app.get('/decode/:encodedPassword', (req, res) => {
+  res.send(Vigenere.Decipher(encodingWord).crypt(req.params.encodedPassword));
+})
+
+app.listen(port);
